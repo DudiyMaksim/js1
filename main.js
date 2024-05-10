53bdda737f3eae8601433e44eb5de54dc8dce3e1
DudiyMaksim <154374761+DudiyMaksim@users.noreply.github.com>
2024-05-10 22:40:56 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+// //task 1
+// let name = prompt("Enter your name")
+// alert("Hello "+ name)
+// //task 2
+// const nuwyear = 2024
+// let birth = +prompt("Enter your year")
+// alert("Your age :"+(nuwyear-birth))
+// // task 3
+// let sidesquere = +prompt("Enter side of squere")
+// alert(sidesquere*4)
+// // task 4
+// let r = +prompt("Enter radius")
+// alert(r**2*3,14)
+// // task 5
+// let S = +prompt("Enter S")
+// let T = +prompt("Enter T")
+// alert(S/T+"km/h")
+// // task 7 
+// let size = +prompt("Enter size")
+// alert((size*1000-(size*1000%820))/820)
+// // task 8
+// let money = +prompt("Enter money")
+// let price = +prompt("Enter price")
+// alert("ypu can buy "+(money-(money%price))/price +" chocolate and you will have "+money%price+" money") 
+// // task 10
+// let a = +prompt("Enter number")
+// if  (a%2==0)
+// {
+//     alert("even")
+// }
+// else 
+// {
+//     alert("odd")
+// }
+// switch (a%2) 
+// {
+//     case 0:
+//         alert("even")
+//         break
+//     default:
+//         alert("odd")
+//         break
+// }
